fbfac29f8c14cd0f0cde72ede498286bb88da6d4
Meenaii <114498271+Meenaii@users.noreply.github.com>
2024-10-11 18:23:49 +0300

    Update Список пересекающихся задач.groovy

--- Scipted fields/Список пересекающихся задач.groovy
+++ Scipted fields/Список пересекающихся задач.groovy
@@ -5,11 +5,11 @@ def issueLinkManager = ComponentAccessor.issueLinkManager
 
 def issuesWithIntersectingLinks = []
 
-def linkedIssues = issueLinkManager.getOutwardLinks(issue.id).collect {it ->
+def linkedIssues = issueLinkManager.getOutwardLinks(issue.id).collect {it -> //Сбор исходящих задач по типу связи
     it.issueLinkType.id == 11000
     it.destinationObject 
 }
-  def intersectingIssues = linkedIssues.each { linkedIssue ->
+  def intersectingIssues = linkedIssues.each { linkedIssue -> //Поиск пересечений связей с исходящей задачей
         def inwardIssues = issueLinkManager.getInwardLinks(linkedIssue.id).findAll{ ln -> 
             if (ln.issueLinkType.id == 11000 && ln.sourceObject.id != issue.id)
             issuesWithIntersectingLinks.add(ln.sourceObject.getKey())
@@ -18,7 +18,7 @@ def linkedIssues = issueLinkManager.getOutwardLinks(issue.id).collect {it ->
 
 if(issuesWithIntersectingLinks == [])
 	return null
-else return issuesWithIntersectingLinks.stream()
-										.distinct()
-										.collect()
-										.toString().substring(1, issuesWithIntersectingLinks.toString().length()-1)
+else return issuesWithIntersectingLinks.stream() //Вывод ключей задач
+						.distinct()
+						.collect()
+						.toString().substring(1, issuesWithIntersectingLinks.toString().length()-1)
